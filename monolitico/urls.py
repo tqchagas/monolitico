ddb9f0eca11ee9c94ca823bd2c1393b8dae2da4c
Thiago Chagas <tqchagas@gmail.com>
2018-11-08 19:03:39 -0200

    heroku deploy

--- monolitico/urls.py
+++ monolitico/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +25,4 @@ urlpatterns = [
     path('hoteis/', include('hoteis.urls')),
     path('vendas/', include('vendas.urls')),
     path('voos/', include('voos.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
